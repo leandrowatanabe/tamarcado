6718cbf15ee5837358a142b2a21095d91919068f
Leandro Watanabe <setsuo.leandro@gmail.com>
2022-03-28 20:00:15 -0300

    Commit: Enxugando toda as views com o Generics

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -1,7 +1,23 @@
+from http import client
+from django.utils import timezone
 from rest_framework import serializers
 
-class AgendamentoSerializar(serializers.Serializer):
-    data_horario = serializers.DateTimeField()
-    nome_cliente = serializers.CharField(max_length=200)
-    email_cliente = serializers.EmailField()
-    telefone_cliente = serializers.CharField(max_length=20)
+from agenda.models import Agendamento
+
+class AgendamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agendamento
+        fields = ["id", "data_horario", "nome_cliente", "email_cliente", "telefone_cliente"]
+
+    def validate_data_horario(self, value):
+        if(value < timezone.now()):
+            raise serializers.ValidationError("Agendamento não pode ser feito no passado!")
+        return value
+
+    # def validate(self, attrs):
+    #     telefone_cliente = attrs.get("telefone_cliente", "")
+    #     email_cliente = attrs.get("email_cliente")
+
+    #     if(email_cliente.endswith(".br") and telefone_cliente.startswith("")) and not telefone_cliente.client.startswithc("+55"):
+    #         raise serializers.ValidationError("Email brasileiro deve estar associado a um númedo no Brasil.")
+    #     return attrs
--- agenda/tests.py
+++ agenda/tests.py
@@ -1,3 +1,57 @@
-from django.test import TestCase
+from datetime import datetime, timezone
+import json
+from rest_framework.test import APITestCase
+
+from agenda.models import Agendamento
 
 # Create your tests here.
+
+class TestListagemAgendamentos(APITestCase):
+    def test_listagem_vazia(self):
+        response = self.client.get("/api/agendamentos/")
+        data = json.loads(response.content)
+        self.assertEqual(data, [])
+
+    def test_listagem_de_agendamentos_criados(self):
+        Agendamento.objects.create(
+            data_horario=datetime(2022,3,28),
+            nome_cliente="Alice",
+            email_cliente="alice@codar.me",
+            telefone_cliente="12345678",
+        )
+
+        agendamento_serializado = {
+            "id":1,
+            "data_horario":"2022-03-28T00:00:00Z",
+            "nome_cliente":"Alice",
+            "email_cliente":"alice@codar.me",
+            "telefone_cliente":"12345678"
+        }
+        
+        response = self.client.get("/api/agendamentos/")
+        data = json.loads(response.content)
+        self.assertDictEqual(data[0], agendamento_serializado)
+
+class TestCriacaoAgendamento(APITestCase):
+    def test_cria_agendamento(self):
+        # agendamento_request_data = {
+        #     "data_horario":"2022-03-28 00:00:00",
+        #     "nome_cliente":"Alice",
+        #     "email_cliente":"alice@codar.me",
+        #     "telefone_cliente":"12345678",
+        # }
+
+        Agendamento.objects.create(
+            data_horario=datetime(2022,3,28),
+            nome_cliente="Alice",
+            email_cliente="alice@codar.me",
+            telefone_cliente="12345678",
+        )
+
+        # response = self.client.post("/api/agendamentos/", agendamento_request_data, format="json")
+
+        agendamento_criado = Agendamento.objects.get()
+
+        self.assertEqual(agendamento_criado.data_horario, datetime(2022, 3, 28, tzinfo=timezone.utc))
+
+ 
